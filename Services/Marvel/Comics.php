1d6c59be35f4fa1340cf6f58c0b59ad3091c7ce8
Keith Casey <keith@caseysoftware.com>
2014-02-01 02:03:22 -0600

    refactored the rest of the index methods into a base class

--- Services/Marvel/Comics.php
+++ Services/Marvel/Comics.php
@@ -2,19 +2,6 @@
 
 class Services_Marvel_Comics extends Services_Marvel_Resources_List
 {
-    public function index($page = 1, $limit = 100)
-    {
-        $page = max($page, 1);
-        $limit = min($limit, 100);
-        $offset = ($page - 1) * $limit;
+    protected $resource = 'comics';
 
-        $params = array('offset' => $offset, 'limit' => $limit);
-
-        $this->client->get($this->client->getUri() . 'comics', $params);
-
-        $this->total = $this->client->response_obj->data->total;
-        $this->count = $this->client->response_obj->data->count;
-
-        return $this->client->response_obj->data->results;
-    }
 }
--- Services/Marvel/Resources/List.php
+++ Services/Marvel/Resources/List.php
@@ -1,6 +1,6 @@
 <?php
 
-class Services_Marvel_Resources_List
+abstract class Services_Marvel_Resources_List
 {
     protected $client = null;
 
@@ -10,5 +10,21 @@ class Services_Marvel_Resources_List
     public function __construct(Services_Marvel $client)
     {
         $this->client = $client;
+    }
+
+    public function index($page = 1, $limit = 100)
+    {
+        $page = max($page, 1);
+        $limit = min($limit, 100);
+        $offset = ($page - 1) * $limit;
+
+        $params = array('offset' => $offset, 'limit' => $limit);
+
+        $this->client->get($this->client->getUri() . $this->resource, $params);
+
+        $this->total = $this->client->response_obj->data->total;
+        $this->count = $this->client->response_obj->data->count;
+
+        return $this->client->response_obj->data->results;
     }
 }
