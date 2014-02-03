34ffb450a132aee3838987ed9c523ba7245c45ec
Keith Casey <keith@caseysoftware.com>
2014-02-03 01:00:15 -0600

    added a helper method for special situations

---
+++ examples/characters-assemble.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once '../creds.php';
+include_once '../Services/Marvel.php';
+
+$client = new Services_Marvel($public_key, $private_key);
+
+try {
+    $team = $client->characters->assemble();
+
+    foreach($team as $member) {
+        echo $member->name . "\n";
+    }
+} catch (Exception $exc) {
+    echo $exc->getMessage() . "\n";
+}
