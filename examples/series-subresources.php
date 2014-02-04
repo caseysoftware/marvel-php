579d06fea54294c6300d4a9086c990ea0f3afac7
Keith Casey <keith@caseysoftware.com>
2014-02-03 20:42:32 -0600

    wired the series subresources

---
+++ examples/series-subresources.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once '../creds.php';
+include_once '../Services/Marvel.php';
+
+$client = new Services_Marvel($public_key, $private_key);
+
+try {
+    $series = $client->series->get(454);
+    echo $series->title . "\n";
+
+    $comics = $series->comics();
+    echo $comics->available . "\t" . $comics->collectionURI . "\n";
+
+    $stories = $series->stories();
+    echo $stories->available . "\t" . $stories->collectionURI . "\n";
+
+    $events = $series->events();
+    echo $events->available . "\t" . $events->collectionURI . "\n";
+
+    $characters = $series->characters();
+    echo $characters->available . "\t" . $characters->collectionURI . "\n";
+
+    $creators = $series->creators();
+    echo $creators->available . "\t" . $creators->collectionURI . "\n";
+} catch (Exception $exc) {
+    echo $exc->getMessage() . "\n";
+}
