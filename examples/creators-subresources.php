d00b341aadf1475de8eb85149176a9fc4761ad32
Keith Casey <keith@caseysoftware.com>
2014-02-03 20:28:25 -0600

    wired the creator sub resources

---
+++ examples/creators-subresources.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once '../creds.php';
+include_once '../Services/Marvel.php';
+
+$client = new Services_Marvel($public_key, $private_key);
+
+try {
+    $creator = $client->creators->get(5251);
+    echo $creator->firstName . ' ' . $creator->lastName . "\n";
+
+    $comics = $creator->comics();
+    echo $comics->available . "\t" . $comics->collectionURI . "\n";
+
+    $series = $creator->series();
+    echo $series->available . "\t" . $series->collectionURI . "\n";
+
+    $stories = $creator->stories();
+    echo $stories->available . "\t" . $stories->collectionURI . "\n";
+
+    $events = $creator->events();
+    echo $events->available . "\t" . $events->collectionURI . "\n";
+} catch (Exception $exc) {
+    echo $exc->getMessage() . "\n";
+}
