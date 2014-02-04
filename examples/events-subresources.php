ebf296802684757085a651705d8ef2f0797187e9
Keith Casey <keith@caseysoftware.com>
2014-02-03 20:33:48 -0600

    wired the events subresources

---
+++ examples/events-subresources.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once '../creds.php';
+include_once '../Services/Marvel.php';
+
+$client = new Services_Marvel($public_key, $private_key);
+
+try {
+    $event = $client->events->get(238);
+    echo $event->title . "\n";
+
+    $comics = $event->comics();
+    echo $comics->available . "\t" . $comics->collectionURI . "\n";
+
+    $series = $event->series();
+    echo $series->available . "\t" . $series->collectionURI . "\n";
+
+    $stories = $event->stories();
+    echo $stories->available . "\t" . $stories->collectionURI . "\n";
+
+    $characters = $event->characters();
+    echo $characters->available . "\t" . $characters->collectionURI . "\n";
+
+    $creators = $event->creators();
+    echo $creators->available . "\t" . $creators->collectionURI . "\n";
+} catch (Exception $exc) {
+    echo $exc->getMessage() . "\n";
+}
